Exposing and taking into consideration count include pad as argument for avg pool op. Adding tests and correcting the calculation of output size when window is starting in the right padded area.

tests/ttnn/unit_tests/operations/pool/test_avgpool2d.py
@@ -21,7 +21,7 @@ def randomize_tensor(tensor_map, tensor_shape):
     if tensor_shape in tensor_map.keys():
         torch_tensor = tensor_map[tensor_shape]
     else:
-        torch_tensor = torch.rand(tensor_shape, dtype=torch.bfloat16)
+        torch_tensor = torch.ones(tensor_shape, dtype=torch.bfloat16)
     return torch_tensor
 
 
@@ -33,6 +33,7 @@ def run_avg_pool2d(
     stride,
     padding,
     ceil_mode,
+    count_include_pad,
     divisor_override,
     count_include_pad,
     shard_scheme,
@@ -69,6 +70,7 @@ def run_avg_pool2d(
         stride=stride,
         padding=padding,
         ceil_mode=ceil_mode,
+        count_include_pad=count_include_pad,
         divisor_override=divisor_override,
         count_include_pad=count_include_pad,
         memory_config=ttnn.DRAM_MEMORY_CONFIG,
@@ -125,7 +127,10 @@ def run_avg_pool2d(
 )
 @pytest.mark.parametrize(
     "count_include_pad",
-    [True, False],
+    [
+        False,
+        True,
+    ],
 )
 @pytest.mark.parametrize(
     "divisor_override",